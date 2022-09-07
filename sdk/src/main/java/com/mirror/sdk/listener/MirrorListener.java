617d3a46f84e5726b3baa11039cc44d4df05d38b
Squall <2573040560@qq.com>
2022-09-07 19:15:38 +0800

    add login method for unity

--- sdk/src/main/java/com/mirror/sdk/listener/MirrorListener.java
+++ sdk/src/main/java/com/mirror/sdk/listener/MirrorListener.java
@@ -5,10 +5,6 @@ import com.mirror.sdk.response.market.NFTObject;
 
 public class MirrorListener {
 
-    public interface LoginListener{
-        void onLoginSuccess();
-        void onLoginFail();
-    }
     public interface FetchUserListener{
         void onUserFetched(UserResponse userResponse);
         void onFetchFailed(long code,String message);
