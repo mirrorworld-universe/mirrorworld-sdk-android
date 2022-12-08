5434dbceccd2c331c9cbda91b3388326de8e2b5f
Squall <2573040560@qq.com>
2022-12-08 13:06:40 +0800

    change value meaning

--- sdk/src/main/java/com/mirror/sdk/particle/MirrorSafeAPI.java
+++ sdk/src/main/java/com/mirror/sdk/particle/MirrorSafeAPI.java
@@ -1,5 +1,7 @@
 package com.mirror.sdk.particle;
 
+import android.util.Log;
+
 import com.google.gson.reflect.TypeToken;
 import com.mirror.sdk.MirrorSDK;
 import com.mirror.sdk.constant.MirrorResCode;
@@ -12,6 +14,8 @@ import com.mirror.sdk.utils.MirrorGsonUtils;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.math.BigDecimal;
 
 public class MirrorSafeAPI {
     final public static void getSecurityToken(String type, String message, int value, JSONObject request, MirrorCallback callback){
@@ -29,17 +33,86 @@ public class MirrorSafeAPI {
         });
     }
 
+    final private static void handleValue(JSONObject request,JSONObject requestParams) throws JSONException {
+        Object amountObj = null;
+        if(requestParams.has("amount")){
+            amountObj = requestParams.get("amount");
+        }
+
+        Object priceObj = null;
+        if(requestParams.has("price")) {
+            priceObj = requestParams.get("price");
+        }
+
+        BigDecimal valueValue = null;
+        int valueCount = 0;
+        if(amountObj != null){
+            valueValue = new BigDecimal(String.valueOf(amountObj));
+            valueCount++;
+        }
+        if(priceObj != null){
+            valueValue = new BigDecimal(String.valueOf(priceObj));
+            valueCount++;
+        }
+
+        if(valueCount == 0){
+            return;
+        }
+
+        if(valueCount > 1){
+            Log.e("MirrorSDK","There is both exists amount and price!");
+            return;
+        }
+
+        String valueKey = "value";
+        if(request.has(valueKey)) {
+            request.remove(valueKey);
+            request.put(valueKey,valueValue.toString());
+        }else {
+            Log.e("MirrorSDK","No value param when approving!");
+        }
+    }
+
     final private static void requestActionAuthorization(String type, String message, int value, JSONObject requestPrams){
+//        JSONObject jsonObject = new JSONObject();
+//        try {
+//            jsonObject.put("mint_address", "1111");
+//            jsonObject.put("price", 1.2);
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
+//
+//
+//        JSONObject aaa = new JSONObject();
+//        try {
+//            aaa.put("type", type);
+//            aaa.put("message", message);
+//            aaa.put("value", value);
+//            aaa.put("params",requestPrams);
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
+//
+//        String baseStr = "{\"collection_mint\":\"DUuMbpmH3oiREntViXfGZhrLMbVcYBwGeBa4Wn9X8QfM\",\"name\":\"1\",\"symbol\":\"1\",\"url\":\"https:\\/\\/metadata-assets.mirrorworld.fun\\/mirror_jump\\/metadata\\/1.json\"}\n";
+//        try {
+//            JSONObject baseObj = new JSONObject(baseStr);
+//            handleValue(aaa,jsonObject);
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
+//        Log.d("MirrorSDK","adfasdf");
         JSONObject jsonObject = new JSONObject();
         try {
             jsonObject.put("type", type);
             jsonObject.put("message", message);
             jsonObject.put("value", value);
             jsonObject.put("params",requestPrams);
+            handleValue(jsonObject,requestPrams);
         } catch (JSONException e) {
             e.printStackTrace();
         }
         String data = jsonObject.toString();
+        MirrorSDK.getInstance().logFlow("request auth data"+data);
 
         String url = MirrorSDK.getInstance().getActionRoot() + MirrorUrl.URL_ACTION_REQUEST;
         MirrorSDK.getInstance().checkParamsAndPost(url,data,MirrorSDK.getInstance().getHandlerCallback(new MirrorCallback() {
